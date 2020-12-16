c808014af5cdf438d3668a707f95a7d55fd6a30d
dharmitviradia <dharmit.viradia@gmail.com>
2020-12-16 13:06:33 +0530

    mongofinal

--- src/components/mongoconnection.js
+++ src/components/mongoconnection.js
@@ -1,40 +1,22 @@
-const {MongoClient} = require('mongodb');
-async function main(){
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-     */
-    const uri = "mongodb+srv://stonkmarket:m0ng0.db@cluster0.ao3cp.mongodb.net/stonkmarket?retryWrites=true&w=majority";
- 
-
-    const client = new MongoClient(uri);
- 
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
- 
-        // Make the appropriate DB calls
-        await  listDatabases(client);
- 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
+const MongoClient = require('mongodb').MongoClient
+const settings = {
+  mongoConfig: {
+    serverUrl: 'mongodb+srv://stonkmarket:m0ng0.db@cluster0.ao3cp.mongodb.net/stonkmarket?retryWrites=true&w=majority',
+    database: 'stonkmarket'
+  }
 }
+const mongoConfig = settings.mongoConfig;
 
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
- 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
-main().catch(console.error);
+let _connection = undefined
+let _db = undefined
 
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://stonkmarket:m0ng0.db@cluster0.ao3cp.mongodb.net/stonkmarket?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("stonkmarket").collection("companies");
-//   client.close();
-// });
+module.exports = async () => {
+  if (!_connection) {
+    _connection = await MongoClient.connect(mongoConfig.serverUrl, {
+      useNewUrlParser: true
+    })
+    _db = await _connection.db(mongoConfig.database)
+  }
+
+  return _db
+}
