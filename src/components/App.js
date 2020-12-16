c94b898b9e0210fc876da10153d2e79b201c2a3d
yashnavadiya <ynavadiya18@gmail.com>
2020-12-16 11:49:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import '../App.css';
 import SignIn from '../components/SignIn'
 import HomePage from '../components/HomePage'
+import Profile from '../components/Profile'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <Router>
         <Route exact path='/' component={HomePage}></Route>
         <Route exact path='/sign-in' component={SignIn}></Route>
-        <Route exact path='/home-page' component={HomePage}></Route>
+        <Route exact path='/profile' component={Profile}></Route>
       </Router>
 
     </div>
