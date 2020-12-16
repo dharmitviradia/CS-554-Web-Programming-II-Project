7680fe945885ae3ccf20abc6ff9cc00a74c9207d
dharmitviradia <dharmit.viradia@gmail.com>
2020-12-16 10:33:49 +0530

    mongodb on cloud

---
+++ src/components/mongoconnection.js
@@ -0,0 +1,40 @@
+const {MongoClient} = require('mongodb');
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://stonkmarket:m0ng0.db@cluster0.ao3cp.mongodb.net/stonkmarket?retryWrites=true&w=majority";
+ 
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+main().catch(console.error);
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://stonkmarket:m0ng0.db@cluster0.ao3cp.mongodb.net/stonkmarket?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("stonkmarket").collection("companies");
+//   client.close();
+// });
