8741cf6964280d94ad7a848074558407fd12c55a
smartdev007 <kevadiajenish7@gmail.com>
2020-12-18 01:05:19 -0500

    Add IPO Calendar

--- Client/src/components/Ipo.js
+++ Client/src/components/Ipo.js
@@ -5,67 +5,68 @@ import { Button } from '@material-ui/core';
 
 const Ipo = () => {
   
-  const [endOffset, setEndOffset] = useState(4);
+  const [endOffset, setEndOffset] = useState(8);
   const [ipoData, setIpoData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  // state = { endOffset: 9 };
   useEffect(() => {
-    axios.get('https://finnhub.io/api/v1/calendar/ipo?from=2020-01-01&to=2020-04-30&token=bvbv2mn48v6rqg57nvcg').then(res => {
-      let ipoDataRes = res.data;
-      setIpoData(ipoDataRes.ipoCalendar);
-    });
+    async function fetchData() {
+        try {
+            const {data} = await axios.get(`https://finnhub.io/api/v1/calendar/ipo?from=2020-01-01&to=2020-04-30&token=bvbv2mn48v6rqg57nvcg`);
+            setIpoData(data);
+            setLoading(false);
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
+    fetchData();
   }, []);
 
+  console.log('here',ipoData)
   const renderIPOData = () => {
-    return ipoData.map(data => {
+    return ipoData.ipoCalendar.slice(0, endOffset).map(data => {
       return (
-        <div key={data.id} className="">
-          <p>
-            <b>Date: </b>{data.date}
-          </p>
-          <p>
-            <b>Exchange: </b>{data.exchange}
-          </p>
-          <p>
-            <b>Name: </b>{data.name}
-          </p>
-          <p>
-            <b>Number of shares: </b>{data.numberOfShares}
-          </p>
-          <p>
-            <b>Price: </b>{data.price}
-          </p>
-          <p>
-            <b>Status: </b>{data.status}
-          </p>
-          <p>
-            <b>Symbol: </b>{data.symbol}
-          </p>
-          <p>
-            <b>Total shares value: </b>{data.totalSharesValue}
-          </p>
+        <div key={data.id}>
+            <figure class="snip1529">
+                <img className="ipo-image" src="https://cdn.wallpapersafari.com/56/10/aLXqcy.jpg" alt={data.id} />
+                <div class="date"><span class="day" style={{padding: 5}}>{data.date}</span></div>
+
+                <figcaption>
+                    <h3>{data.name}</h3>
+                    <p>Exchange: {data.exchange || 'NA'}</p>
+                    <p>Price: {data.price || 'NA'}</p>
+                    <p>Number of Shares: {data.numberOfShares || 'NA'}</p>
+                    <p>Status: {data.status || 'NA'}</p>
+                    <p>Symbol: {data.symbol || 'NA'}</p>
+                    <p>Total Shares Value: {data.totalSharesValue || 'NA'}</p>
+                </figcaption>
+
+                <div class="hover"><i class="ion-android-open"></i></div>
+                <a href={data.url} target="_blank"></a>
+            </figure>
         </div>
       );
     });
   }
 
-  return (
-    <div>
-      <p className="market-news-data-single-list-container-heading">IPO Calendar Data</p>
-      {
-        renderIPOData()
-      }
-      <Button 
-        onClick={() => setEndOffset(endOffset+5)} 
-        variant="contained" 
-        color="primary"
-        fullWidth
-      >
-        Show More
-      </Button>
-    </div>
-  );
-  
+  if (loading) {
+      return (
+          <div>
+              Loading...
+          </div>
+      )
+  } else return (
+            <div className="news-container">
+                <div className="news-wrapper">
+                    {renderIPOData()}
+                </div>
+
+            <div onClick={() => setEndOffset(endOffset+8)} className="load-more">
+                Read More
+            </div>
+            </div>
+        );
 };
 
 export default Ipo;
